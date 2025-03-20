6371aeae94151f3b8d3ff885d8427cff07b25527
Abdulkabeer555 <akgct123@gmail.com>
2025-03-20 19:28:21 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import AOS from 'aos';
+import React, { useEffect, useState } from 'react';
 import 'aos/dist/aos.css';
 import Index from './components/index';
 import About from './components/about';
@@ -8,31 +7,33 @@ import Events from './components/events';
 import Contact from './components/contact';
 
 const App = () => {
-  const path = window.location.pathname;  
-  
-  let Component;
-  if (path === "/about") {
-    Component = About;
-  } else if
-   (path === "/courses") {
-     Component = Courses;
-  } else if
-   (path === "/events") {
-    Component = Events;
-  } 
-   else if
-   (path === "/contact") {
-    Component = Contact;
-  } else {
-    Component = Index;
-  }
+  const [Component, setComponent] = useState(<Index />); // Default component
 
-  
+  useEffect(() => {
+    AOS.init();
+
+    const path = window.location.hash;
+
+    let Component;
+    if (path === "#/about") {
+      Component = <About />;
+    } else if (path === "#/courses") {
+      Component = <Courses />;
+    } else if (path === "#/events") {
+      Component = <Events />;
+    } else if (path === "#/contact") {
+      Component = <Contact />;
+    } else {
+      Component = <Index />;
+    }
+
+    setComponent(Component);
+
+  }, [window.location.hash]); // Re-run whenever hash changes
 
   return (
-    
     <div>
-      <Component />
+      {Component}
     </div>
   );
 };
