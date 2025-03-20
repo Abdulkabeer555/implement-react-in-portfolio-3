df1435536234cd9171f4cbd654a2555474b779dc
Abdulkabeer555 <akgct123@gmail.com>
2025-03-20 19:47:46 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/implement-react-in-portfolio-3/',
+  base: '/implement-react-in-portfolio/',
   plugins: [react()],
 })
